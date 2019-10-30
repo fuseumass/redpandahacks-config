d3b091cf0b1b96e2b387ef10770f33dd8d04240c
abochnak <abochnak@umass.edu>
2019-10-29 21:25:52 -0400

    update logo, css, js

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -1,9 +1,24 @@
 // Custom javascript code which is run on every page.
 
+var addBanner = function() {
+    if (location.hostname.indexOf('staging') != -1) {
+        var e = document.createElement('div');
+        e.classList.add('env-banner');
+        e.innerHTML = 'STAGING';
+        document.body.appendChild(e);
+    } else if (location.hostname.indexOf('hackher413.com') == -1 && location.hostname.indexOf('hackher-dashboard') == -1) {
+        var e = document.createElement('div');
+        e.classList.add('env-banner');
+        e.innerHTML = 'NON-PROD';
+        document.body.appendChild(e);
+    }
+};
+
+
 var init = function() {
-    console.log("This is custom javascript run on every pageload.");
+    // console.log("This is custom javascript run on every pageload.");
+    addBanner();
 }
 
 window.addEventListener('load', init);
 $(document).on('turbolinks:load', init);
-
